b6700f4e776ffcf2de9287e3ac505bb96bd0847e
욱재 <uckjae@naver.com>
2020-07-24 04:01:36 +0900

    extends springbootserverinitializer

--- src/main/java/com/jojoidu/book/springboot/Application.java
+++ src/main/java/com/jojoidu/book/springboot/Application.java
@@ -2,10 +2,11 @@ package com.jojoidu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
     }
